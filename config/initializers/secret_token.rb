70ca1e46a6a207d636526b44eef1e3f8b14c9dcd
Tanapol Nearunchorn <stackle_Sharp@hotmail.com>
2012-09-03 11:24:24 +0700

    Initial Project File

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BoxOffice::Application.config.secret_token = '17042d75e1ec61b41ff2b02ecfbe8a5cf4a43c4f20c89512a443ebb0196cbd7946c83b2d760007e1e5153315c8ed0d9d049106030591e5ea3bf2dcc39a3ad56f'
